165427c03b00d88dc4e87b98c14deb83d8ad263b
Phellipe <phbrasiliano@gmail.com>
2017-06-25 15:51:53 +0200

    now users are automatically logged in after signup. tests added

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -24,7 +24,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password: "password",
                                          password_confirmation: "password" } }
     end
-  follow_redirect!
-  assert_template 'users/show'
+    follow_redirect!
+    assert_template 'users/show'
+    assert is_logged_in?
   end
 end
