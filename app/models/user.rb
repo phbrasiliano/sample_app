4f75238561e66e3b5ca1aa20e84ddebbb2e827c5
Phellipe <phbrasiliano@gmail.com>
2017-06-28 00:56:24 +0200

    adds the remember token function to the user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save { self.email = self.email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -20,4 +22,9 @@ class User < ApplicationRecord
     SecureRandom.urlsafe_base64
   end
   
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
 end
