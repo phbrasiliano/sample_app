68d6890e79053840029baee276fa3d711b3983fd
Phellipe <phbrasiliano@gmail.com>
2017-07-01 00:54:40 +0200

    adds several improvements to the remember log in system

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,8 +27,13 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
-  def authenticated?
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 
 end
